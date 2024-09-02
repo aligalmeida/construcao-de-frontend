043e966e61064e72bc6187701f0e913000e3473c
Aline Almeida <aligalmeida@hotmail.com>
2024-09-02 20:40:46 -0300

    Adicionando arquivos da pratica04

---
+++ aulas/aula03/src/assets/components/Cabecalho.jsx
@@ -0,0 +1,12 @@
+import { useDeferredValue } from "react"
+
+
+function Cabecalho() {
+    return (
+      <header>
+        <h1>Minha Aplicação</h1>
+      </header>
+    )
+    
+    }
+export default App;
--- praticas/pratica1/index.js
+++ praticas/pratica1/index.js
@@ -1,14 +1,33 @@
-const http = require('http');
+const root = document.getElementById("root");
 
-// Cria um servidor HTTP
-const server = http.createServer((req, res) => {
-  res.statusCode = 200; // Define o status como 200 (OK)
-  res.setHeader('Content-Type', 'text/plain'); // Define o tipo de conteúdo como texto
-  res.end('Olá, Pessoal!\n'); // Envia a resposta
-});
+/* componentes */
+function Cabecalho() {
+    const header = document.createElement("header");
+    return header;
+}
 
-// Define a porta em que o servidor irá escutar
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}/`);
-});
+function Conteudo() {
+    const main = document.createElement("main");
+    return main;
+}
+
+function Rodape() {
+    const footer = document.createElement("footer");
+    return footer;
+}
+
+/* paginas */
+function PageLogin() {
+    const conteudo = Conteudo();
+    const rodape = Rodape();
+    root.append(conteudo, rodape);
+}
+
+function PageHome() {
+    const cabecalho = Cabecalho();
+    const conteudo = Conteudo();
+    root.append(cabecalho, conteudo);
+}
+
+PageLogin();
+
