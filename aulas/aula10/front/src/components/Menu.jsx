975b10dabef4ea8769a7bc244c608270f3541863
Aline Almeida <aligalmeida@hotmail.com>
2024-10-24 22:00:45 -0300

    Aula 10

--- aulas/aula10/front/src/components/Menu.jsx
+++ aulas/aula10/front/src/components/Menu.jsx
@@ -1,6 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 function Menu() {
-    return (
-    <></>
-    );
+    return <nav>
+        <ul>
+            <li>
+                <NavLink to="/">Listar Contato</NavLink>
+            </li>
+            <li>
+                <NavLink to="/">Novo Contato</NavLink>
+            </li>
+        </ul>
+    </nav>;
 }
 export default Menu;
--- aulas/aula10/front/src/services/ContatoService.js
+++ aulas/aula10/front/src/services/ContatoService.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 
-const uri = import.meta.env.API_URL;
+const url = import.meta.env.VITE_API_URL;
 
 function buscarTodos() {
-
+return axios
+.get(url)
+.then((response) => {
+    return {sucesso: true, dados: response.data };
+})
+.catch((error) => {
+    return { sucesso: false, mensagem: error.message };
+});
 }
 
 function buscar(id) {
@@ -21,6 +28,14 @@ function atualizar (contato) {
 }
 
 function remover (id) {
+    return axios
+    .delete(`${url}/${id}`)
+    .then((reponse) => {
+        return { sucesso: true, dados: responde.data };
+    })
+    .catch((error) => {
+        return { sucesso: false, mensagem: error.message };
+    });
 
 
 }
